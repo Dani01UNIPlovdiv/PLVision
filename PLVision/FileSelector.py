553036cc08446c99beb00e731833b7e655b9d1b4
Dani01UNIPlovdiv <77579847+Dani01UNIPlovdiv@users.noreply.github.com>
2024-05-13 15:42:21 +0300

    Update FileSelector.py

--- PLVision/FileSelector.py
+++ PLVision/FileSelector.py
@@ -13,13 +13,13 @@ import glob  # Import glob
 import os  # Import os
 
 
-def getFile(fileCount, filePath, fileFormat, all=False):  # This function returns the most recent file
-    if all:  # If all is true
-        list_of_files = glob.glob(filePath + fileFormat)
-        list_of_files = [file.replace("storage\\", "storage/") for file in list_of_files]
-        return list_of_files
+def getFile(fileCount, filePath, fileFormat):
+    list_of_files = glob.glob(filePath + fileFormat)  # Get a list of files
+
+    if not list_of_files:  # Check if list_of_files is empty
+        return "No files found that match the given pattern."
+
     for i in range(fileCount):
-        list_of_files = glob.glob(filePath + fileFormat)  # Get a list of files
         latest_file = max(list_of_files, key=os.path.getctime)  # Get the latest file
-        latest_file = latest_file.replace("storage\\", "storage/")  # Remove storage\\ from the string
+        #latest_file = latest_file.replace("storage\\", "storage/")  # Remove storage\\ from the string
         return latest_file  # Return the latest file
