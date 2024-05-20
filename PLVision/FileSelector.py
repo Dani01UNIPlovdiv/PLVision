713a9d1a142e281a8023580daafac62316576bb7
Dani01UNIPlovdiv <77579847+Dani01UNIPlovdiv@users.noreply.github.com>
2024-05-20 16:45:29 +0300

    Update FileSelector.py

--- PLVision/FileSelector.py
+++ PLVision/FileSelector.py
@@ -17,10 +17,11 @@ def getFile(fileCount, filePath, fileFormat, all=False):
     # Get a list of all files
     list_of_files = glob.glob(filePath + fileFormat)
     list_of_files = [file.replace("storage\\", "storage/") for file in list_of_files]
-
     # Sort the list of files based on creation time in descending order
     list_of_files.sort(key=os.path.getctime, reverse=True)
-
+    for file in list_of_files:
+        with open(file, 'r') as f:
+            content = f.read()
     if all:
         # If all is True, return all files
         return list_of_files
